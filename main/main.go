61bb6ac6c211f720d3b1c05ca8d3f41ec3e75240
lixiaolong <lixiaolong2014@xiaochuankeji.cn>
2023-07-02 21:01:39 +0800

    init

--- main/main.go
+++ main/main.go
@@ -6,8 +6,9 @@ import (
 )
 
 func main() {
-	// test
+	// 初始化gin框架
 	r := gin.Default()
+	// 124
 	r.LoadHTMLFiles("html/login.html")
 	r.GET("/home", func(c *gin.Context) {
 		c.HTML(200, "login.html", "flysnow_org")
